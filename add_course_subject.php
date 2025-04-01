b079d02423ff8aec4add6ca947ba95f6af9476c7
Ivan Brilata <brilataivan86@gmail.com>
2025-04-01 22:23:13 +0800

    [comment out] add course check subjects

--- add_course_subject.php
+++ add_course_subject.php
@@ -6,13 +6,13 @@ if (isset($_POST['subject_code'], $_POST['subject_name'], $_POST['course_code'])
     $subject_name = trim($_POST['subject_name']);
     $course_code = trim($_POST['course_code']);
 
-    $check = $conn->prepare("SELECT * FROM subjects WHERE subject_code = ?");
-    $check->execute([$subject_code]);
+    // $check = $conn->prepare("SELECT * FROM subjects WHERE subject_code = ?");
+    // $check->execute([$subject_code]);
 
-    if ($check->rowCount() > 0) {
-        echo json_encode(["status" => "exists"]);
-        exit;
-    }
+    // if ($check->rowCount() > 0) {
+    //     echo json_encode(["status" => "exists"]);
+    //     exit;
+    // }
 
     $stmt = $conn->prepare("INSERT INTO subjects_course (subject_code, subject_name, course_code) VALUES (?, ?, ?)");
     if ($stmt->execute([$subject_code, $subject_name, $course_code])) {
