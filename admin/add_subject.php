5e2bc3e6766a7d3c38984f55b41e7ddfca945044
Ivan Brilata <brilataivan86@gmail.com>
2025-05-06 17:00:02 +0800

    [comment out] subjects

--- admin/add_subject.php
+++ admin/add_subject.php
@@ -6,13 +6,13 @@ if (isset($_POST['subject_code'], $_POST['subject_name'], $_POST['department_cod
     $subject_name = trim($_POST['subject_name']);
     $department_code = trim($_POST['department_code']);
 
-    $check = $conn->prepare("SELECT * FROM subjects WHERE subject_code = ?");
-    $check->execute([$subject_code]);
+    // $check = $conn->prepare("SELECT * FROM subjects WHERE subject_code = ?");
+    // $check->execute([$subject_code]);
 
-    if ($check->rowCount() > 0) {
-        echo json_encode(["status" => "exists"]);
-        exit;
-    }
+    // if ($check->rowCount() > 0) {
+    //     echo json_encode(["status" => "exists"]);
+    //     exit;
+    // }
 
     $stmt = $conn->prepare("INSERT INTO subjects (subject_code, subject_name, department_code) VALUES (?, ?, ?)");
     if ($stmt->execute([$subject_code, $subject_name, $department_code])) {
